52531cacfa4f172ca1f8b0f03c9ff4a5a015e032
alejapiatti <104015514+alejapiatti@users.noreply.github.com>
2022-08-23 16:00:48 +0200

    Home y MainShow

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,7 @@
-/*import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Container} from "react-bootstrap";
 import MainShow from  "../components/MainShow"
 import Login from './Login';
-
-
-
-
-
 
 const Home = () => {
   const [accessToken, setAccessToken] = useState('');
@@ -16,14 +11,23 @@ const Home = () => {
   }, []);
 
   return (
-    <Container>
-    
 
-      {accessToken ? <MainShow /> : <Login/>}
+    <Container class= "bg-#33FFAC text-#3633FF">
+    <div>
+          <h1><strong>Disfruta de la música dondequiera que estés</strong></h1>  
+          </div>
+
+          <div><h2>Escucha las mejores novedades</h2></div>
+
+          <div>
+          <Login></Login>
+          </div>
+      
+    
     </Container>
   )
 };
 
-export default Home;*/
+export default Home;
 
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,10 +5,10 @@ import {Container} from "react-bootstrap";
 
 export default function Login() {
 
-    const CLIENT_ID = "5bc283c16a824cd7afb8b3b5c8c253fd"
+    const CLIENT_ID = "0e03f7856efb4ac1aa1f04a62d6ecb09"
     const REDIRECT_URI = "http://localhost:3000/callback";
     const AUTH_ENDPOINT= 'http://accounts.spotify.com/authorize';
-    const SECRET = '219a5e5d915341e78ef35dda952da9fe'
+    const SECRET = '54e87833c36c49b1b7dab53e9825521a'
     const scopes= ["user-read-email", 
     "user-read-private","user-library-read",
     "user-library-modify","user-read-playblack-state",
@@ -19,7 +19,7 @@ export default function Login() {
        
        <Container className="d-flex justify-content-center align-items-center" style= {{minHeight: "100vh"}}>
         
-        <a className='btn btn-success btn-lg' 
+        <a className='btn btn-success btn-lg bg-#33FFAC text-#3633FF' 
         href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}
         &redirect_uri=${REDIRECT_URI}
         &response_type=token&secret_key=${SECRET}
@@ -29,5 +29,4 @@ export default function Login() {
     )
 
 }
-   
-const 
+    
